c3cf22fe10ca8c63a452cbabfbeceb19c99df48e
416lj <laxsan.jeya@gmail.com>
2025-12-16 21:07:04 -0500

    increase vehicles as levels go up

--- main.py
+++ main.py
@@ -14,31 +14,30 @@ screen.listen()
 score = Scoreboard()
 player = Player()
 screen.onkey(player.up, "Up")
-
 speed = 0.1
-
 car_list = []
-num_of_cars = 200
-for i in range(random.randint(1, num_of_cars)):
-    car_list.append(CarManager())
+num_of_cars = 5
+
+def create_cars():
+    for i in range(num_of_cars):
+        car_list.append(CarManager())
 
 game_is_on = True
 while game_is_on:
     time.sleep(speed)
     screen.update()
-
+    if random.randint(1,6) == 1:
+        create_cars()
     for car in car_list:
         car.move()
         if player.distance(car) < 25:
             game_is_on = False
             score.game_over()
-
     if player.check_finish():
         score.update_score()
-        num_of_cars += 200
         for car in car_list:
             car.car_reset()
         player.reset_turtle()
-        speed *= 0.9
+        speed *= 1.1
 
 screen.exitonclick()
